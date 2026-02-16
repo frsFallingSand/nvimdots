a7aacf9624f9bad9451a2d65f47f8cf9cf5aa7a5
frsFallingSand <frsfallingsand@outlook.com>
2026-02-16 17:11:58 +0800

    feat(lang): gradle

--- lua/modules/plugins/lang.lua
+++ lua/modules/plugins/lang.lua
@@ -114,4 +114,17 @@ lang["AlexandrosAlexiou/kotlin.nvim"] = {
 		})
 	end,
 }
+lang["oclay1st/gradle.nvim"] = {
+	cmd = { "Gradle", "GradleExec", "GradleInit", "GradleFavorites" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+	},
+	opts = {}, -- options, see default configuration
+	keys = {
+		{ "<leader>G", desc = "+Gradle", mode = { "n", "v" } },
+		{ "<leader>Gg", "<cmd>Gradle<cr>", desc = "Gradle Projects" },
+		{ "<leader>Gf", "<cmd>GradleFavorites<cr>", desc = "Gradle Favorite Commands" },
+	},
+}
 return lang
